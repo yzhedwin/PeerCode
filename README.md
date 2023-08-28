ec78ce575518c4f05bb15bfc151ff30a40c52a14
yzhedwin <zizo_edwin@hotmail.com>
2023-08-28 15:39:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CS3219 Project
+## Build
+Run `docker-compose -f docker-compose.yml build`
+
+## Run
+Run `bash start-sh` in project root directory
---
+++ frontend/cs3219-react/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState();
+
+  const getMessage = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/");
+
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getMessage();
+  }, []);
+
+  return <div>{data}</div>;
+}
+
+export default App;
