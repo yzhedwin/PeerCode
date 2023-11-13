ae3f4e1e250a7b6913cbf20d9811c7d5f188c39a
Dun Liang <dunliang014@gmail.com>
2023-11-13 20:57:37 +0800

    Add question status router to server directory

---
+++ backend/server/router/question_status_router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+import requests
+from config import get_config
+from model.question_status import QuestionStatus
+
+router = APIRouter(
+    prefix="/api/v1/question-status",
+    tags=["question-status"],
+    responses={404: {"description": "Not found"}},
+)
+
+config = get_config()
+
+@router.get("")
+async def get_all_question_status(userID: str):
+  try:
+    response = requests.get(config.question_status_service_url, params={"userID": userID})
+    return response.json()
+  except Exception as e:
+    return e
+
+@router.put("")
+async def update_question_status(status: QuestionStatus):
+  try:
+    response = requests.put(config.question_status_service_url, json=status.dict())
+    return response.json()
+  except Exception as e:
+    return e
