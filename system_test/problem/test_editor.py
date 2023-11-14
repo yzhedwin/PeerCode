55e6181c7e17412519cdb7b44780e42530b94eb5
LimAiLin <limailin2009@gmail.com>
2023-11-14 15:16:51 -0500

    editor

--- system_test/problem/test_editor.py
+++ system_test/problem/test_editor.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
 def test_editor_solutions_panel_success():
-    driver = webdriver.Chrome()
-    driver.get('http://localhost:3000/login')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('http://peercode.net/login')
     wait = WebDriverWait(driver, 10)
     try:
         # Login
@@ -18,29 +22,28 @@ def test_editor_solutions_panel_success():
         login_button.click()
 
         # Wait for the next page to load after login
-        WebDriverWait(driver, 10).until(EC.url_to_be('http://localhost:3000/'))
-        assert driver.current_url == 'http://localhost:3000/', "Login failed"
+        WebDriverWait(driver, 10).until(EC.url_to_be('http://peercode.net/dashboard'))
+        assert driver.current_url == 'http://peercode.net/dashboard', "Login failed"
 
         # Find and click the 'Two Sum' element
         two_sum_element = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='title-div' and text()='Two Sum']")))
         two_sum_element.click()
 
         # Wait for redirection to /problem
-        WebDriverWait(driver, 10).until(EC.url_to_be('http://localhost:3000/problem'))
-        assert driver.current_url == 'http://localhost:3000/problem', "Navigation to /problem failed"
+        WebDriverWait(driver, 10).until(EC.url_to_be('http://peercode.net/problem'))
+        assert driver.current_url == 'http://peercode.net/problem', "Navigation to /problem failed"
 
         # Find and click on "Solutions" button
         solutions_button_element = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@id='problempage-full-width-tab-1']")))
         solutions_button_element.click()
+        assert solutions_button_element.get_attribute("aria-selected") == "true"
 
-        # Wait for the solutions container to appear
-        solutions_element = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='MuiStack-root css-nen11g-MuiStack-root']")))
     finally:
         driver.quit()
 
 def test_editor_submissions_panel_success():
-    driver = webdriver.Chrome()
-    driver.get('http://localhost:3000/login')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('http://peercode.net/login')
     wait = WebDriverWait(driver, 10)
     try:
         # Login
@@ -53,23 +56,21 @@ def test_editor_submissions_panel_success():
         login_button.click()
 
         # Wait for the next page to load after login
-        WebDriverWait(driver, 10).until(EC.url_to_be('http://localhost:3000/'))
-        assert driver.current_url == 'http://localhost:3000/', "Login failed"
+        WebDriverWait(driver, 10).until(EC.url_to_be('http://peercode.net/dashboard'))
+        assert driver.current_url == 'http://peercode.net/dashboard', "Login failed"
 
         # Find and click the 'Two Sum' element
         two_sum_element = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='title-div' and text()='Two Sum']")))
         two_sum_element.click()
 
         # Wait for redirection to /problem
-        WebDriverWait(driver, 10).until(EC.url_to_be('http://localhost:3000/problem'))
-        assert driver.current_url == 'http://localhost:3000/problem', "Navigation to /problem failed"
+        WebDriverWait(driver, 10).until(EC.url_to_be('http://peercode.net/problem'))
+        assert driver.current_url == 'http://peercode.net/problem', "Navigation to /problem failed"
 
         # Find and click on "Submissions" button
         submissions_button_element = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@id='problempage-full-width-tab-2']")))
         submissions_button_element.click()
-
-        # Wait for the submissions container to appear
-        submissions_element = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='ag-root ag-unselectable ag-layout-normal']")))
+        assert submissions_button_element.get_attribute("aria-selected") == "true"
 
     finally:
         driver.quit()
