9136190dee399bc8ac2214bf7365b5c32cdad639
LimAiLin <limailin2009@gmail.com>
2023-11-15 06:25:33 +0800

    debug

--- system_test/login/test_login.py
+++ system_test/login/test_login.py
@@ -5,8 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = Options()
-chrome_options.addArguments("--no-sandbox");
-chrome_options.addArguments("--disable-dev-shm-usage");
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--headless")
 
 
