592c29c3ef42e81b4b511767fde5e330645ed682
LimAiLin <limailin2009@gmail.com>
2023-11-14 14:04:11 -0500

    signup system_test

--- system_test/signup/test_signup.py
+++ system_test/signup/test_signup.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-
+'''
 def test_signup_successful():
     driver = webdriver.Chrome(options=chrome_options)
     driver.get('http://localhost:3000/signup')
@@ -84,10 +84,10 @@ def test_signup_failure_email_in_use():
         assert driver.current_url == 'http://localhost:3000/signup', "URL mismatch"
     finally:
         driver.quit()
-
+'''
 def test_signup_failure_password_mismatch():
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get('http://localhost:3000/signup')
+    driver.get('http://peercode.net/signup')
     wait = WebDriverWait(driver, 10)
 
     try:
@@ -109,12 +109,80 @@ def test_signup_failure_password_mismatch():
         # Simulate a click using JavaScript
         driver.execute_script("arguments[0].click();", sign_up_button)
 
-        snackbar = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='MuiSnackbar-root MuiSnackbar-anchorOriginTopCenter css-zzms1-MuiSnackbar-root']")))
+        snackbar = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='MuiSnackbar-root MuiSnackbar-anchorOriginTopCenter css-186hw1j']")))
         assert snackbar.text == 'Password mismatched', "Snackbar Text mismatch"
 
         # Wait for the success message or redirect
-        WebDriverWait(driver, 10).until(EC.url_to_be('http://localhost:3000/signup'))
+        WebDriverWait(driver, 10).until(EC.url_to_be('http://peercode.net/signup'))
 
-        assert driver.current_url == 'http://localhost:3000/signup', "URL mismatch"
+        assert driver.current_url == 'http://peercode.net/signup', "URL mismatch"
+    finally:
+        driver.quit()
+
+def test_signup_failure_existing_user():
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('http://peercode.net/signup')
+    wait = WebDriverWait(driver, 10)
+
+    try:
+        username = wait.until(EC.presence_of_element_located((By.NAME, "username")))
+        email = wait.until(EC.presence_of_element_located((By.NAME, "email")))
+        password = wait.until(EC.presence_of_element_located((By.NAME, "password")))
+        confirm_password = wait.until(EC.presence_of_element_located((By.NAME, "confirmPassword")))
+        sign_up_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Sign Up']")))
+
+        # Fill in the registration form
+        username.send_keys('testing')
+        email.send_keys('test@test.com')
+        password.send_keys('testing1!')
+        confirm_password.send_keys('testing1!')  # Fill in the password confirmation
+
+        # Scroll to the element
+        driver.execute_script("arguments[0].scrollIntoView(true);", sign_up_button)
+        
+        # Simulate a click using JavaScript
+        driver.execute_script("arguments[0].click();", sign_up_button)
+
+        snackbar = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='MuiSnackbar-root MuiSnackbar-anchorOriginTopCenter css-186hw1j']")))
+        assert snackbar.text == 'Email is already in use', "Snackbar Text mismatch"
+
+        # Wait for the success message or redirect
+        WebDriverWait(driver, 10).until(EC.url_to_be('http://peercode.net/signup'))
+
+        assert driver.current_url == 'http://peercode.net/signup', "URL mismatch"
+    finally:
+        driver.quit()
+
+def test_signup_failure_insecure_pwd():
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('http://peercode.net/signup')
+    wait = WebDriverWait(driver, 10)
+
+    try:
+        username = wait.until(EC.presence_of_element_located((By.NAME, "username")))
+        email = wait.until(EC.presence_of_element_located((By.NAME, "email")))
+        password = wait.until(EC.presence_of_element_located((By.NAME, "password")))
+        confirm_password = wait.until(EC.presence_of_element_located((By.NAME, "confirmPassword")))
+        sign_up_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Sign Up']")))
+
+        # Fill in the registration form
+        username.send_keys('testing')
+        email.send_keys('test@test.com')
+        password.send_keys('testing1!')
+        confirm_password.send_keys('testing1!')  # Fill in the password confirmation
+
+        # Scroll to the element
+        driver.execute_script("arguments[0].scrollIntoView(true);", sign_up_button)
+        
+        # Simulate a click using JavaScript
+        driver.execute_script("arguments[0].click();", sign_up_button)
+
+        snackbar = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='MuiSnackbar-root MuiSnackbar-anchorOriginTopCenter css-186hw1j']")))
+        assert snackbar.text == 'Password must have at least 1 number and 1 non-alphanumeric character', "Snackbar Text mismatch"
+
+        # Wait for the success message or redirect
+        WebDriverWait(driver, 10).until(EC.url_to_be('http://peercode.net/signup'))
+
+        assert driver.current_url == 'http://peercode.net/signup', "URL mismatch"
     finally:
         driver.quit()
