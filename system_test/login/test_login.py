09cb4362b625d6d50f413c00bd300e1bce9cb36d
LimAiLin <limailin2009@gmail.com>
2023-11-15 06:43:26 +0800

    test

--- system_test/login/test_login.py
+++ system_test/login/test_login.py
@@ -3,6 +3,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import chromedriver_autoinstaller
+
+chromedriver_autoinstaller.install()
 
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
